6a837228972cc5e4ac39afbbdd1368708ba2df29
Shannon Skipper <shannonskipper@gmail.com>
2013-09-28 12:44:36 -0700

    Initial commit.

--- lib/era.rb
+++ lib/era.rb
@@ -1,5 +1,18 @@
-require "era/version"
+require 'era/version'
 
 module Era
-  # Your code goes here...
+  ##
+  # Evaluates a block and prints the time it took. Returns the block value.
+  # Takes an optional argument for number of times to run the block.
+  def self.time this_many = 1
+    value = nil
+    ms = Benchmark.realtime do
+      this_many.pred.times do
+        yield
+      end
+      value = yield
+    end
+    puts "Elapsed time: #{ms} ms"
+    value
+  end
 end
